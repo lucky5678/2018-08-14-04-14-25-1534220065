23729b2a19661dbfce7dd51f85375d9bbc8ff62e
zhujunjun <1712477640@qq.com>
2019-05-24 16:43:59 +0800

    first

--- main/main.js
+++ main/main.js
@@ -1,4 +1,45 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(number) {
+    let stringsStyles = {
+        0: ["._.", "|.|", "|_|"],
+        1: ["...", "..|", "..|"],
+        2: ["._.", "._|", "|_."],
+        3: ["._.", "._|", "._|"],
+        4: ["...", "|_|", "..|"],
+        5: ["._.", "|_.", "._|"],
+        6: ["._.", "|_.", "|_|"],
+        7: ["._.", "..|", "..|"],
+        8: ["._.", "|_|", "|_|"],
+        9: ["._.", "|_|", "..|"]
+    }
+    
+    function splitNum(number) {
+        return number.split("");
+    }
+    
+    
+    function matchingStyle(numbers, stringsStyle) {
+        let stringSet = [];
+    
+        for (let i = 0; i < 3; i++) {
+            let string = numbers.reduce((str, item) => {
+                str += `${stringsStyle[item][i]} `;
+                return str;
+            }, "");
+            stringSet.push(`${string.substring(0, string.length - 1)}\n`);
+        }
+    
+        return stringSet;
+    }
+    
+    function printDigit(stringSet) {
+    
+        return stringSet.reduce((pString, cString) => {
+            pString += cString;
+            return pString;
+        })
+    }
+    
+    let numbers = splitNum(number);
+    let stringSet = matchingStyle(numbers, stringsStyles);
+    console.log(printDigit(stringSet));
 };
